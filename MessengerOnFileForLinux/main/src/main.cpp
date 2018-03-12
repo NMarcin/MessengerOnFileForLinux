d0ff79be5ce2528867a3c025be1d68ca64e6a1e1
NMarcin <marcin.nurzynski97@gmail.com>
2018-03-13 00:31:25 +0100

    main -> code checker

--- MessengerOnFileForLinux/main/src/main.cpp
+++ MessengerOnFileForLinux/main/src/main.cpp
@@ -9,20 +9,15 @@
 #include <ClasslessLogger.hpp>
 #include <LogSpace.hpp>
 
-double squareRoot(const double a) {
-    double b = sqrt(a);
-    if(b != b)
-    {
-        return -1.0;
-    }
-    else
-    {
-        return sqrt(a);
-    }
+void mnurzyns()
+{
+    std::cout << "mnurzyns:\n\n";
+    fileLog("Witamy w logerze!", LogSpace::main);
 }
 
-int main(int argc, char **argv)
+void mwozniak()
 {
+    std::cout << "mwozniak:\n\n";
     /*
     createFile("plik.txt", "../");
     createFile("tmp.txt");
@@ -37,8 +32,34 @@ int main(int argc, char **argv)
     signIn.signInUser();
     SignOut signOut;
     signOut.signOutUser();
+}
 
-    fileLog("Witamy w logerze!", LogSpace::main);
+enum class Run
+{
+    mnurzyns,
+    mwozniak,
+    both
+};
+
+int main(int argc, char **argv)
+{
+    Run version = Run::mnurzyns;
+
+    if (Run::mnurzyns == version)
+    {
+        mnurzyns();
+    }
+    else if (Run::mwozniak == version)
+    {
+        mwozniak();
+    }
+    else if (Run::both == version)
+    {
+        mnurzyns();
+        std::cout << std::endl << std::endl << std::endl;
+        mwozniak();
+    }
+
 
     return 0;
 }
