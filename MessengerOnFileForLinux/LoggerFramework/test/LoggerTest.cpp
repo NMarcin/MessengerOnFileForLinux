6acf66a9917aa4d686e9a4abc46cb436dc955c2b
NMarcin <marcin.nurzynski97@gmail.com>
2018-03-22 20:57:48 +0100

    strzelam, że to Ci naprawi

--- MessengerOnFileForLinux/LoggerFramework/test/LoggerTest.cpp
+++ MessengerOnFileForLinux/LoggerFramework/test/LoggerTest.cpp
@@ -8,48 +8,48 @@
 
 TEST(LoggerFrameworkTest, logFileCreation)
 {
+    FileInterface::Managment::removeFile("Logger_default.txt");
+    
     EXPECT_FALSE(FileInterface::Managment::isFileExist("Logger_default.txt"));
     Logger log(LogSpace::Logger);
     log.info("Test log");
     EXPECT_TRUE(FileInterface::Managment::isFileExist("Logger_default.txt"));
-
-    FileInterface::Managment::removeFile("Logger_default.txt");
 }
 
 TEST(LoggerFrameworkTest, logFileCreationWithoutClass)
 {
+    FileInterface::Managment::removeFile("Logger_default.txt");
+    
     EXPECT_FALSE(FileInterface::Managment::isFileExist("Logger_default.txt"));
     fileLog("Test log", LogSpace::Logger);
     EXPECT_TRUE(FileInterface::Managment::isFileExist("Logger_default.txt"));
-
-    FileInterface::Managment::removeFile("Logger_default.txt");
 }
 
 TEST(LoggerFrameworkTest, writingToLogFile)
 {
+    FileInterface::Managment::removeFile("Logger_default.txt");
+    
     Logger log(LogSpace::Logger);
     log.info("Test log");
     std::ifstream logFile("Logger_default.txt", std::ifstream::ate);
     int fileSize = logFile.tellg();
-
     EXPECT_TRUE(fileSize);
-
-    FileInterface::Managment::removeFile("Logger_default.txt");
 }
 
 TEST(LoggerFrameworkTest, writingToLogFileWithoutClass)
 {
+    FileInterface::Managment::removeFile("Logger_default.txt");
+    
     fileLog("Test log", LogSpace::Logger);
     std::ifstream logFile("Logger_default.txt", std::ifstream::ate);
     int fileSize = logFile.tellg();
-
     EXPECT_TRUE(fileSize);
-
-    FileInterface::Managment::removeFile("Logger_default.txt");
 }
 
 TEST(LoggerFrameworkTest, isLogCorrectlyAdded)
 {
+    FileInterface::Managment::removeFile("Logger_default.txt");
+    
     Logger log(LogSpace::Logger);
     std::string logInfo = "Test log";
     log.info(logInfo);
@@ -63,12 +63,12 @@ TEST(LoggerFrameworkTest, isLogCorrectlyAdded)
     logInFile.erase(logInFile.begin(), logInFile.begin() + DateTimeSize);
 
     EXPECT_EQ(expectedLog, logInFile);
-
-    FileInterface::Managment::removeFile("Logger_default.txt");
 }
 
 TEST(LoggerFrameworkTest, isLogCorrectlyAddedWithoutClass)
 {
+    FileInterface::Managment::removeFile("Logger_default.txt");
+    
     std::string logInfo = "Test log";
     fileLog(logInfo.c_str(), LogSpace::Logger);
     std::string expectedLog = "Logger \t " + logInfo;
@@ -81,12 +81,12 @@ TEST(LoggerFrameworkTest, isLogCorrectlyAddedWithoutClass)
     logInFile.erase(logInFile.begin(), logInFile.begin() + DateTimeSize);
 
     EXPECT_EQ(expectedLog, logInFile);
-
-    FileInterface::Managment::removeFile("Logger_default.txt");
 }
 
 TEST(LoggerFrameworkTest, isLogNotOverwritten)
 {
+    FileInterface::Managment::removeFile("Logger_default.txt");
+    
     Logger log(LogSpace::Logger);
     log.info("Test log");
     std::ifstream logFile("Logger_default.txt", std::ifstream::ate);
@@ -99,6 +99,4 @@ TEST(LoggerFrameworkTest, isLogNotOverwritten)
 
 
     EXPECT_TRUE(newFileSize > fileSize);
-
-    FileInterface::Managment::removeFile("Logger_default.txt");
 }
