447fe330770812055945130ff30ffa35f90c002e
marcinwozniak0 <wozniak1008@gmail.com>
2018-04-18 00:46:28 +0200

    Working on UserServices UT

---
+++ MessengerOnFileForLinux/UserService/test/UserTest.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include <User.hpp>
+#include <LocalUser.hpp>
+
+TEST(UserTest, setUpUsernameByCtor)
+{
+    User user("mwoznia2");
+    EXPECT_EQ("mwoznia2", user.getUsername());
+}
+
+TEST(UserTest, isPossibleSetUsername)
+{
+    User user;
+    EXPECT_EQ("", user.getUsername());
+    user.setUsername("mnurzyns");
+    EXPECT_NE("", user.getUsername());
+    EXPECT_EQ("mnurzyns", user.getUsername());
+}
+
+TEST(UserTest, isSetPasswordWorksCorretly)
+{
+    User user;
+    EXPECT_EQ("", user.getPassword());
+    user.setPassword("123456789");
+    EXPECT_NE("", user.getPassword());
+    EXPECT_EQ("123456789", user.getPassword());
+}
+
+TEST(LocalUserTest, isLocalUsernameIsCorrect)
+{
+    std::string localUsername = getenv("USER");
+    EXPECT_EQ(localUsername, LocalUser::getLocalUser().getUsername());
+}
