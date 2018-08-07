6614446ec5f4648d628c632d36d39b0789fcf4fe
NMarcin <marcin.nurzynski97@gmail.com>
2018-08-07 23:01:30 +0200

    Receiver first flow

--- MessengerOnFileForLinux/ChatControl/src/Receiver.cpp
+++ MessengerOnFileForLinux/ChatControl/src/Receiver.cpp
@@ -1,32 +1,68 @@
-#include "Receiver.hpp"
+#include <Receiver.hpp>
 
-Receiver::Receiver()
+#include <GlobalVariables.hpp>
+#include <LocalUser.hpp>
+
+Reciver::Reciver()
 {
     //NOOP
 }
 
-Receiver::~Receiver()
+Reciver::~Reciver()
 {
     //NOOP
 }
 
+bool Reciver::readMessages()
+{
+// BLOKUJEMY DOSTĘP DO FOLDERU
 
-bool Receiver::getRawMessagesFromFile(std::string chatFileWithPath)
+    std::unique_ptr<std::vector<std::string>> messagesFileContent = FileInterface::Accesor::getFileContent(chatFileWithPath);
+    auto fileContentIterator = messagesFileContent->end();
+    while(fileContentIterator != messagesFileContent->begin()) // dany wiersz jest do odbiorcy oraz został już przeczytany wcześniej
+    {
+        --fileContentIterator;
+        int messageFlag;
+        messageFlag = std::atoi((FileInterface::Accesor::getRowField(*fileContentIterator, FileStructure::LoggedFile::username))->c_str()); // LoggedFile::username podmienic na odpowiedni stworzony przez mnowznia
+        if( mineMessageUserFlag != messageFlag)
+        {
+            std::string purgeMessage;
+            purgeMessage = purgeMessageFromRaw(*fileContentIterator);
+            purgeMessagesStack.push(purgeMessage);
+        }
+        if( 0 == messageFlag)
+        {
+            std::string senderUsername;
+            senderUsername = *FileInterface::Accesor::getRowField(*fileContentIterator, FileStructure::LoggedFile::username); // LoggedFile::username podmienic na odpowiedni stworzony przez mnowznia
+            if( LocalUser::getLocalUser().getUsername() == senderUsername)
+            {
+                break;
+            }
+        }
+    }
+    //updateFlagsInFile()
+
+// ODBLOKOWUJEMY DOSTĘP DO FOLDERU
+
+    return false;
+}
+
+bool Reciver::getRawMessagesFromFile(std::string chatFileWithPath)
 {
     return false;
 }
 
-std::string Receiver::getMessageFromStack()
+std::string Reciver::getMessageFromStack()
 {
     return "false";
 }
 
-std::string Receiver::purgeMessage(std::string rawMessage)
+std::string Reciver::purgeMessageFromRaw(std::string rawMessage)
 {
     return "false";
 }
 
-bool Receiver::addMessageToQueue(std::queue<std::string> messagesToShowOnScreen, std::string purgeMessage)
+bool Reciver::addMessageToQueue(std::queue<std::string> messagesToShowOnScreen, std::string purgeMessage)
 {
     return false;
 }
--- MessengerOnFileForLinux/ChatControl/src/Receiver.hpp
+++ MessengerOnFileForLinux/ChatControl/src/Receiver.hpp
@@ -3,25 +3,31 @@
 #include <queue>
 #include <string>
 /*
+- pobieramy cały plik
+- blokujemy dostęp do pliku
+-
+
 - pobieramy od końca pliku linijki które mają odpowiednią flagę na początku
 - uzywamy do tego stosu, bo potem w odwrotnej kolejnosci bedziemy chcieli wrzucac do kolejki
 - nastepnie z tego stosu zabieram po linijce i oprawiamy ja i zwracamy w innej funkcji
 - kolejna funkcja wrzuca oprawiona linijke do naszej kolejki do wyswietlania
 */
 
-class Receiver
+class Reciver
 {
 public:
-    Receiver();
-    ~Receiver();
+    Reciver();      // chatFileWithPath -> do konstruktora
+    ~Reciver();
 
     bool readMessages();
 
 private:
     bool getRawMessagesFromFile(std::string chatFileWithPath);                                          // pobiera wszystkie linijki NEW z pliku
     std::string getMessageFromStack();                                                                  // pobiera wiadomość ze stosu i ją zwraca
-    std::string purgeMessage(std::string rawMessage);                                                   // otrzymanego stringa oczyszcza na stringa, ktory zostanie wyświetlony na odpowiednim ekranie
+    std::string purgeMessageFromRaw(std::string rawMessage);                                                   // otrzymanego stringa oczyszcza na stringa, ktory zostanie wyświetlony na odpowiednim ekranie
     bool addMessageToQueue(std::queue<std::string> messagesToShowOnScreen, std::string purgeMessage);   // otrzymanego stringa wrzuca do kolejki, na którą ma wskaźnik, tej z Chat Controla
 
-    std::stack<std::string> newRawMessages;
+    const std::string chatFileWithPath;
+    std::stack<std::string> purgeMessagesStack;
+    int mineMessageUserFlag;
 };
