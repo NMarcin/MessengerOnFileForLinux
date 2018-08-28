a77cbe30e88369a14106840d1385453f39984405
NMarcin <marcin.nurzynski97@gmail.com>
2018-08-28 21:29:53 +0200

    part of ChatControl copy-pasted to TerminalControl

--- MessengerOnFileForLinux/ChatControl/src/TerminalControl.cpp
+++ MessengerOnFileForLinux/ChatControl/src/TerminalControl.cpp
@@ -1,2 +1,53 @@
+#include<TerminalControl.hpp>
 
-// nie dodane do kompilowania
+#include <ChatRequest.hpp>
+// REMEMBER TO ADD THIS FILE INTO TEST CMAKELISTS
+
+void TerminalControl::startConversation(const std::string& username, ChatRole chatRole)
+{
+    log.info(("ChatControl::startConversation started whit chatRole = " + std::to_string(static_cast<int>(chatRole))).c_str());
+    if (ChatRole::inviter == chatRole)
+    {
+        startConversationAsInviter(username);
+    }
+    else if (ChatRole::recipient == chatRole)
+    {
+        startConversationAsRecipient(username);
+    }
+}
+
+void TerminalControl::startConversationAsInviter(const std::string& username)
+{
+    log.info("ChatControl::startConversationAsInviter started");
+    ChatRequest chatRequest;
+    chatFileWithPath_ = chatRequest.sendChatRequest(username);
+    if(!chatFileWithPath_.empty())
+    {
+        std::string info = "ChatControl::startConversationAsInviter chatFileWithPath_: " + chatFileWithPath_;
+        log.info(info.c_str());
+        messageFlag_ = MessageFlag::inviterMessage;
+        //conversation(); // TO DO mnurzyns zwraca boola i gdzie indziej obsluga converstaion()
+    }/*
+    else
+    {
+        conversationEpilog();
+    }*/
+}
+
+void TerminalControl::startConversationAsRecipient(const std::string& username)
+{
+    log.info("ChatControl::startConversationAsRecipient started");
+    ChatRequest chatRequest;
+    chatFileWithPath_ = chatRequest.answerForChatRequest(username);
+    if(!chatFileWithPath_.empty())
+    {
+        std::string info = "ChatControl::startConversationAsRecipient chatFileWithPath_: " + chatFileWithPath_;
+        log.info(info.c_str());
+        messageFlag_ = MessageFlag::recipientMessage;
+        //conversation(); // TO DO mnurzyns zwraca boola i gdzie indziej obsluga converstaion()
+    }/*
+    else
+    {
+        conversationEpilog();
+    }*/
+}
--- MessengerOnFileForLinux/ChatControl/src/TerminalControl.hpp
+++ MessengerOnFileForLinux/ChatControl/src/TerminalControl.hpp
@@ -1,5 +1,22 @@
 #pragma once
+
+#include <Logger.hpp>
+#include <LogSpace.hpp>
 // nie dodane do kompilowania
+
+
+enum class MessageFlag
+{
+    readMessage,
+    inviterMessage,
+    recipientMessage
+};
+
+enum class ChatRole // TO DO mnurzyns usunac jak nie bedzie potrzebne
+{
+    inviter,
+    recipient
+};
 
 class TerminalControl //: public ChatControl
 {
@@ -7,7 +24,12 @@ public:
     void waitinginTerminal();
 
 private:
-    void conversationProlog(const std::string& username, ChatRole chatRole);
+    void startConversation(const std::string& username, ChatRole chatRole);
     void startConversationAsInviter(const std::string& username);
     void startConversationAsRecipient(const std::string& username);
-}
+
+    std::string chatFileWithPath_;
+    MessageFlag messageFlag_;
+
+    Logger log{LogSpace::ChatFile};
+};
