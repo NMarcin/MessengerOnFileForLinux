07ddb636cbb0c9f5c5036e301461a32a4e85cf3a
NMarcin <marcin.nurzynski97@gmail.com>
2018-08-31 15:29:13 +0200

    chatcontrol should died

--- MessengerOnFileForLinux/ChatControl/src/ChatControl.hpp
+++ MessengerOnFileForLinux/ChatControl/src/ChatControl.hpp
@@ -30,32 +30,11 @@ class ChatControl
 public:
     ChatControl();
     ~ChatControl();
-    void conversationProlog(const std::string& username, ChatRole chatRole);
-    void conversationEpilog();
 
 private:
-    void conversation();
-    void getMessage();
-    void reciveMessage();
-    void startConversationAsInviter(const std::string& username);
-    void startConversationAsRecipient(const std::string& username);
-    void sendMessage();
-    void stopThreads();
-
-    std::unique_ptr<std::thread> getMessageToQueueThread_;
-    std::unique_ptr<std::thread> sendMessageFromQueueThread_;
-    std::unique_ptr<std::thread> reciveMessageThread_;
-
-    std::queue<std::string> messageReadyToSend_;
-    std::queue<std::string> messageToDisplay_;
 
     std::string chatFileWithPath_;
     MessageFlag messageFlag_;
-    bool isThreadsRunning_;
-
-    WINDOW* displayMessageWindow_;                                              // TODO mwozniak to powinno być jako klasa, która udostępniainterfejs, a nie część ChatControlu
-    WINDOW* enterMessageWindow_;
 
     Logger log{LogSpace::ChatFile};
-
 };
