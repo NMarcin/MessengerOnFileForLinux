79654954e655a65026db1c7d8936c18318f6ac9b
marcinwozniak0 <wozniak1008@gmail.com>
2019-01-10 12:25:31 +0100

    Add build job to GitLab CI

---
+++ MessengerOnFileForLinux/scripts/gcc_ci.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /builds/NMarcin/MessengerOnFileForLinux/MessengerOnFileForLinux/
+mkdir build
+cd build
+cmake ..
+make
---
+++ MessengerOnFileForLinux/scripts/setup_ci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt-get update --yes
+wget http://www.cmake.org/files/v3.10/cmake-3.10.1.tar.gz
+tar -xvzf cmake-3.10.1.tar.gz
+cd cmake-3.10.1/
+./configure
+make 
+make install
+apt-get install --yes libgtest-dev
+cd /usr/src/gtest
+cmake CMakeLists.txt
+make
+cp *.a /usr/lib
+apt-get update
+apt-get install --yes ncurses-dev
+apt install --yes libncurses5-dev libncursesw5-dev
