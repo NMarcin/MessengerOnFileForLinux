9e92edd8ec1392f3e27e076620875622dbadf20b
marcinwozniak0 <wozniak1008@gmail.com>
2019-03-10 15:37:56 +0100

    Add script to check clang-tidy job status

--- MessengerOnFileForLinux/scripts/check_cland-tidy_job_status.sh
+++ MessengerOnFileForLinux/scripts/check_cland-tidy_job_status.sh
@@ -3,7 +3,7 @@
 CLANG_TIDY_OUTPUT_FILE=$1
 COMMAND=`grep warning $CLANG_TIDY_OUTPUT_FILE | wc -l`
 if [ $COMMAND -eq 0 ] ; then
-    touch no_clang-tidy_warnigns
+    mkdir no_clang-tidy_warnigns
 fi
-cat no_clang-tidy_warnigns
+cd no_clang-tidy_warnigns
 
