323dd06da73767935f56774baf6f07e0cc6f2324
marcinwozniak0 <wozniak1008@gmail.com>
2019-03-10 16:13:02 +0100

    Attempt to fail cland-tidy job

--- MessengerOnFileForLinux/scripts/check_cland-tidy_job_status.sh
+++ MessengerOnFileForLinux/scripts/check_cland-tidy_job_status.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 CLANG_TIDY_OUTPUT_FILE=$1
-COMMAND=`grep warning $CLANG_TIDY_OUTPUT_FILE | wc -l`
+#COMMAND=`grep warning $CLANG_TIDY_OUTPUT_FILE | wc -l`
+COMMAND=5
 echo $COMMAND
 if [ $COMMAND -eq 0 ] ; then
     mkdir no_clang-tidy_warnigns
