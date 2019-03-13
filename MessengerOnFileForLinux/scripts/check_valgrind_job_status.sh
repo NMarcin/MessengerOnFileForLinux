c9512a29b535d64985910b63554d04ea19f6b567
marcinwozniak0 <36385922+marcinwozniak0@users.noreply.github.com>
2019-03-14 00:05:40 +0100

    Update check_valgrind_job_status.sh

--- MessengerOnFileForLinux/scripts/check_valgrind_job_status.sh
+++ MessengerOnFileForLinux/scripts/check_valgrind_job_status.sh
@@ -6,6 +6,5 @@ ERROR_NUMBER_POSITION_IN_ERROR_SUMARY=25
 ERROR_NUMBER=${VALGRIND_ERROR_SUMARY:$ERROR_NUMBER_POSITION_IN_ERROR_SUMARY:1}
 echo $ERROR_NUMBER
 if [ $ERROR_NUMBER -eq 0 ] ; then
-    mkdir no_valgrind_warnigns
+    exit 1
 fi
-cd no_valgrind_warnigns
