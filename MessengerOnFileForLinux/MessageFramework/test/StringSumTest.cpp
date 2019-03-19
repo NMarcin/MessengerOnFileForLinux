8f87745dbbee9dc33cdd39a5fcda3d20fb9fd1ea
mnurzyns <marcin.nurzynski@nokia.com>
2019-03-19 14:22:53 +0100

    Test case for StringSum
    One test case is enought because StringSum class give us
    only interface to sum small string with brackets betwixt.

--- MessengerOnFileForLinux/MessageFramework/test/StringSumTest.cpp
+++ MessengerOnFileForLinux/MessageFramework/test/StringSumTest.cpp
@@ -1,7 +1,14 @@
 #include <gtest/gtest.h>
 #include <StringSum.hpp>
 
-TEST(StringSumTest, trueIsTrue)
+TEST(StringSumTest, sumString)
 {
-    EXPECT_TRUE(true);
+    StringSumSquareBrackets summedString;
+    std::string expectedString = "[This ][string ][is][ summed.]";
+    summedString.sum("This ");
+    summedString.sum("string ");
+    summedString.sum("is");
+    summedString.sum(" summed.");
+
+    EXPECT_EQ(summedString.getSumedString(), expectedString);
 }
