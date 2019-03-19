ef2bd84bb04cce42e66f2a8ee653eb3f31e958c1
marcinwozniak0 <wozniak1008@gmail.com>
2019-03-19 10:26:06 +0100

    Minnor changes in LocalUserTest

--- MessengerOnFileForLinux/UserService/test/LocalUserTest.cpp
+++ MessengerOnFileForLinux/UserService/test/LocalUserTest.cpp
@@ -6,13 +6,13 @@ TEST(LocalUserTest, isSetPasswordWorksCorretly)
 {
     EXPECT_EQ("", LocalUser::getLocalUser().getPassword());
     LocalUser::getLocalUser().setPassword("123456789");
-    EXPECT_NE("", LocalUser::getLocalUser().getPassword());
+
     EXPECT_EQ("123456789", LocalUser::getLocalUser().getPassword());
 }
 
 TEST(LocalUserTest, isLocalUsernameIsCorrect)
 {
-    std::string localUsername = getenv("USER");
+    const std::string localUsername = getenv("USER");
     EXPECT_EQ(localUsername, LocalUser::getLocalUser().getUsername());
 }
 
