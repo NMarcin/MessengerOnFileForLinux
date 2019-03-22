2a20c07287a1644495a92214941690c0989a2485
mnurzyns <marcin.nurzynski@nokia.com>
2019-03-22 09:23:33 +0100

    Add const in Message class test cases
    Aligment to low prio comment in review

--- MessengerOnFileForLinux/MessageFramework/test/MessageTest.cpp
+++ MessengerOnFileForLinux/MessageFramework/test/MessageTest.cpp
@@ -7,9 +7,9 @@ TEST_F(MessageFixture, isMessageCorrectCreatedFromFullMessageInRow)
 
 TEST_F(MessageFixture, isMessageCorrectCreatedFromParameters)
 {
-    Message readMessage("[0][2019-03-21 08:41:30][mnurzyns][test read message]");
-    Message inviterMessage("[1][2019-03-21 08:41:30][mnurzyns][test inviter message]");
-    Message recipientMessage("[2][2019-03-21 08:41:30][mnurzyns][test recipient message]");
+    const Message readMessage("[0][2019-03-21 08:41:30][mnurzyns][test read message]");
+    const Message inviterMessage("[1][2019-03-21 08:41:30][mnurzyns][test inviter message]");
+    const Message recipientMessage("[2][2019-03-21 08:41:30][mnurzyns][test recipient message]");
     EXPECT_EQ(*expectedReadMessage, readMessage.messageToSave());
     EXPECT_EQ(*expectedInviterMessage, inviterMessage.messageToSave());
     EXPECT_EQ(*expectedRecipientMessage, recipientMessage.messageToSave());
@@ -17,11 +17,11 @@ TEST_F(MessageFixture, isMessageCorrectCreatedFromParameters)
 
 TEST_F(MessageFixture, isTimeFromMessageCorrectReturned)
 {
-    Message readMessage(*expectedReadMessage);
-    Message inviterMessage(*expectedInviterMessage);
-    Message recipientMessage(*expectedRecipientMessage);
+    const Message readMessage(*expectedReadMessage);
+    const Message inviterMessage(*expectedInviterMessage);
+    const Message recipientMessage(*expectedRecipientMessage);
 
-    std::string expectedTime = "2019-03-21 08:41:30";
+    const std::string expectedTime = "2019-03-21 08:41:30";
 
     EXPECT_EQ(expectedTime, readMessage.getTime());
     EXPECT_EQ(expectedTime, inviterMessage.getTime());
@@ -30,11 +30,11 @@ TEST_F(MessageFixture, isTimeFromMessageCorrectReturned)
 
 TEST_F(MessageFixture, isUsernameFromMessageCorrectReturned)
 {
-    Message readMessage(*expectedReadMessage);
-    Message inviterMessage(*expectedInviterMessage);
-    Message recipientMessage(*expectedRecipientMessage);
+    const Message readMessage(*expectedReadMessage);
+    const Message inviterMessage(*expectedInviterMessage);
+    const Message recipientMessage(*expectedRecipientMessage);
 
-    std::string expectedUsername = "mnurzyns";
+    const std::string expectedUsername = "mnurzyns";
 
     EXPECT_EQ(expectedUsername, readMessage.getUsername());
     EXPECT_EQ(expectedUsername, inviterMessage.getUsername());
@@ -43,13 +43,13 @@ TEST_F(MessageFixture, isUsernameFromMessageCorrectReturned)
 
 TEST_F(MessageFixture, isContentFromMessageCorrectReturned)
 {
-    Message readMessage(*expectedReadMessage);
-    Message inviterMessage(*expectedInviterMessage);
-    Message recipientMessage(*expectedRecipientMessage);
+    const Message readMessage(*expectedReadMessage);
+    const Message inviterMessage(*expectedInviterMessage);
+    const Message recipientMessage(*expectedRecipientMessage);
 
-    std::string expectedReadContent = "test read message";
-    std::string expectedInviterContent = "test inviter message";
-    std::string expectedRecipientContent = "test recipient message";
+    const std::string expectedReadContent = "test read message";
+    const std::string expectedInviterContent = "test inviter message";
+    const std::string expectedRecipientContent = "test recipient message";
 
     EXPECT_EQ(expectedReadContent, readMessage.getContent());
     EXPECT_EQ(expectedInviterContent, inviterMessage.getContent());
