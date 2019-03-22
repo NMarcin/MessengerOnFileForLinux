4c872a6488087033153832035e221cbe22f6b427
marcinwozniak0 <36385922+marcinwozniak0@users.noreply.github.com>
2019-03-22 18:55:21 +0100

    Update setup_ci.sh

--- MessengerOnFileForLinux/scripts/setup_ci.sh
+++ MessengerOnFileForLinux/scripts/setup_ci.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-apt-get update --yes
-#wget http://www.cmake.org/files/v3.10/cmake-3.10.1.tar.gz
-#tar -xvzf cmake-3.10.1.tar.gz
-#cd cmake-3.10.1/
-#./configure
-#make 
-#make install
-#apt-get install --yes cmake=3.1*
+
+#CMAKE INSTALLATION
 wget -qO- "https://cmake.org/files/v3.14/cmake-3.14.0-Linux-x86_64.tar.gz" | tar --strip-components=1 -xz -C /usr/local
-apt-get update
-apt-get install --yes ncurses-dev
-apt install --yes libncurses5-dev libncursesw5-dev
-apt-get install --yes valgrind
+
+#UBUNTU PACKAGES
 apt-get update --yes
-apt-get install --yes python-setuptools
+apt-get install --yes ncurses-dev\
+                      libncurses5-dev\
+                      libncursesw5-dev\
+                      valgrind\
+                      doxygen\
+                      graphviz\
+                      lcov\
+                      clang\
+                      clang-tidy\
+                      python-setuptools
+                      
+#PYTHON PACKAGES NEEDED FOR CLANG-TIDY                       
 easy_install pip
-apt-get install --yes clang
-apt-get install --yes clang-tidy
-apt-get install --yes libyaml-dev
-apt-get install --yes python3-dev
-apt-get install --yes python-dev
+apt-get install --yes libyaml-dev\
+                      python3-dev\
+                      python-dev\
 pip install pyyaml
-apt-get install --yes doxygen
-apt-get install --yes graphviz
-apt-get install --yes lcov
+
