bd827637c9a67a8fc22f6361f2469e0d4e1cf79d
marcinwozniak0 <36385922+marcinwozniak0@users.noreply.github.com>
2019-03-22 18:40:01 +0100

    Update setup_ci.sh

--- MessengerOnFileForLinux/scripts/setup_ci.sh
+++ MessengerOnFileForLinux/scripts/setup_ci.sh
@@ -6,7 +6,8 @@ apt-get update --yes
 #./configure
 #make 
 #make install
-apt-get install --yes cmake=3.1*
+#apt-get install --yes cmake=3.1*
+wget -qO- "https://cmake.org/files/v3.14/cmake-3.14.0-Linux-x86_64.tar.gz" | tar --strip-components=1 -xz -C /usr/local
 apt-get update
 apt-get install --yes ncurses-dev
 apt install --yes libncurses5-dev libncursesw5-dev
