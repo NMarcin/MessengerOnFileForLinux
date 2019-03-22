d7258494303fdc479e942319ad4521020ce33ae2
marcinwozniak0 <36385922+marcinwozniak0@users.noreply.github.com>
2019-03-23 00:24:35 +0100

    Update setup_ci.sh

--- MessengerOnFileForLinux/scripts/setup_ci.sh
+++ MessengerOnFileForLinux/scripts/setup_ci.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-apt-get update --yes
+#CMAKE INSTALLATION
 wget -qO- "https://cmake.org/files/v3.14/cmake-3.14.0-Linux-x86_64.tar.gz" | tar --strip-components=1 -xz -C /usr/local
 apt-get install --yes ncurses-dev
-apt install --yes libncurses5-dev libncursesw5-dev
+apt-get install --yes libncurses5-dev 
+apt-get install --yes libncursesw5-dev
 apt-get install --yes valgrind
 apt-get install --yes python-setuptools
 apt-get install --yes clang
@@ -15,4 +16,3 @@ apt-get install --yes graphviz
 apt-get install --yes lcov
 easy_install pip
 pip install pyyaml
-
