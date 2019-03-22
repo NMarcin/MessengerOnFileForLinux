fa2d5b7def9efa32ec25c59ad23683a4fd8c493e
marcinwozniak0 <wozniak1008@gmail.com>
2019-03-23 00:48:30 +0100

    INT: Refactor setup_ci script and add missed export in this script

--- MessengerOnFileForLinux/scripts/ci_scripts/setup_ci_environment.sh
+++ MessengerOnFileForLinux/scripts/ci_scripts/setup_ci_environment.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-apt-get update --yes
+#CMAKE INSTALLATION
 wget -qO- "https://cmake.org/files/v3.14/cmake-3.14.0-Linux-x86_64.tar.gz" | tar --strip-components=1 -xz -C /usr/local
-apt-get update
-apt-get install --yes ncurses-dev
-apt install --yes libncurses5-dev libncursesw5-dev
-apt-get install --yes valgrind
+
+#NEEDED UBUNTU PACKAGES
 apt-get update --yes
+apt-get install --yes ncurses-dev
+apt-get install --yes libncurses5-dev 
+apt-get install --yes libncursesw5-dev
+apt-get install --yes valgrind
 apt-get install --yes python-setuptools
-easy_install pip
 apt-get install --yes clang
 apt-get install --yes clang-tidy
 apt-get install --yes libyaml-dev
 apt-get install --yes python3-dev
 apt-get install --yes python-dev
-pip install pyyaml
 apt-get install --yes doxygen
 apt-get install --yes graphviz
 apt-get install --yes lcov
+
+#NEEDED PYTHON PACKAGES
+easy_install pip
+pip install pyyaml
+
+#EXPORTS
+export USER=ciGitlab
+export CXX=/usr/local/bin/g++
+export PROJECT_DIR=/builds/NMarcin/MessengerOnFileForLinux/MessengerOnFileForLinux
+
