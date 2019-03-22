b8757f06f741f5de8ac3c85aa140212f3f36b620
marcinwozniak0 <36385922+marcinwozniak0@users.noreply.github.com>
2019-03-22 19:40:40 +0100

    Update setup_ci.sh

--- MessengerOnFileForLinux/scripts/setup_ci.sh
+++ MessengerOnFileForLinux/scripts/setup_ci.sh
@@ -3,17 +3,18 @@
 wget -qO- "https://cmake.org/files/v3.14/cmake-3.14.0-Linux-x86_64.tar.gz" | tar --strip-components=1 -xz -C /usr/local
 #UBUNTU PACKAGES
 apt-get update --yes
-apt-get install --yes ncurses-dev
-apt install --yes libncurses5-dev libncursesw5-dev
-apt-get install --yes valgrind
-apt-get install --yes doxygen
-apt-get install --yes graphviz
-apt-get install --yes lcov
-apt-get install --yes clang
-apt-get install --yes clang-tidy
-apt-get install --yes libyaml-dev
-apt-get install --yes python3-dev
-apt-get install --yes python-dev
+apt-get install --yes ncurses-dev\
+libncurses5-dev\ 
+libncursesw5-dev\
+valgrind\
+doxygen\
+graphviz\
+lcov\
+clang\
+clang-tidy\
+libyaml-dev\
+python3-dev\
+python-dev
 
 #PYTHON PACKAGES NEEDED FOR CLANG-TIDY
 #apt-get update --yes
